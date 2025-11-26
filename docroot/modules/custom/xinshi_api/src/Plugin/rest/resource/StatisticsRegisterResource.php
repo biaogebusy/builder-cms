3a1ee9d58b41ae8dab8f790a43e76a7818c981b4
donkblongsi <donkblongsi@yeah.net>
2025-11-26 10:28:59 +0800

    更新统计API

--- docroot/modules/custom/xinshi_api/src/Plugin/rest/resource/StatisticsRegisterResource.php
+++ docroot/modules/custom/xinshi_api/src/Plugin/rest/resource/StatisticsRegisterResource.php
@@ -32,6 +32,7 @@ class StatisticsRegisterResource extends XinshibResourceBase {
       // triggers the code that we don't don't control that in turn triggers early rendering.
       return $this->getRegister($request);
     });
+    $this->addCacheTags(['user_list']);
     return $this->getResponse($data);
   }
 
@@ -41,7 +42,6 @@ class StatisticsRegisterResource extends XinshibResourceBase {
    * @return array
    */
   private function getRegister(Request $request) {
-
     $rang = CommonUtil::getYearRange();
     $date = $rang['date'];
     $statistics = $this->statisticsUserRegister($request, $rang['date'], $rang['end_date']);
@@ -57,6 +57,7 @@ class StatisticsRegisterResource extends XinshibResourceBase {
       $chart[] = $row;
     }
     return [
+      'total' => $this->statisticsUserRegisteTotal($request),
       'count' => $count,
       'chart' => $chart,
     ];
@@ -74,7 +75,7 @@ class StatisticsRegisterResource extends XinshibResourceBase {
     $query->addExpression("FROM_UNIXTIME(users_field_data.created, '%Y%m')", 'month');
     $query->addExpression('count(users_field_data.uid)', 'total');
     $query->condition('users_field_data.status', 1);
-    $role = \Drupal::request()->get('role') ? explode(' ', $request->get('role')) : [];
+    $role = $request->get('role') ? explode(' ', $request->get('role')) : [];
     $opposite = $request->get('opposite');
     if ($role || $opposite) {
       $sub_query = $db->select('user__roles', 'user__roles');
@@ -101,5 +102,38 @@ class StatisticsRegisterResource extends XinshibResourceBase {
     return $data;
   }
 
-
+  /**
+   * 统计注册用户总数
+   *
+   * 根据请求参数统计符合条件的活跃用户总数，支持按角色筛选和反向筛选
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request 包含筛选条件的请求对象
+   *   - role: 可选，以空格分隔的角色ID字符串，用于筛选具有指定角色的用户
+   *   - opposite: 可选，布尔值，为true时筛选不包含指定角色的用户
+   *
+   * @return int 符合条件的用户总数
+   */
+  private function statisticsUserRegisteTotal(Request $request) {
+    $db = \Drupal::database();
+    $query = $db->select('users_field_data', 'users_field_data');
+    $query->addExpression('count(users_field_data.uid)', 'total');
+    $query->condition('users_field_data.status', 1);
+    $role = $request->get('role') ? explode(' ', $request->get('role')) : [];
+    $opposite = $request->get('opposite');
+    if ($role || $opposite) {
+      $sub_query = $db->select('user__roles', 'user__roles');
+      $sub_query->addExpression('DISTINCT(user__roles.entity_id)', 'entity_id');
+      if ($role) {
+        $sub_query->condition('user__roles.roles_target_id', $role, 'in');
+      }
+      $sub_query->condition('user__roles.deleted', 0);
+      $query->leftJoin($sub_query, 'user__roles', 'users_field_data.uid=user__roles.entity_id');
+      if ($opposite) {
+        $query->condition('user__roles.entity_id', '', 'is null');
+      } else {
+        $query->condition('user__roles.entity_id', '', 'is not null');
+      }
+    }
+    return (int) $query->execute()->fetchField();
+  }
 }
