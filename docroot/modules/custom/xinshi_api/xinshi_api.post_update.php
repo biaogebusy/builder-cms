cc550a389e170ea207192e507afb45750e0085db
donkblongsi <donkblongsi@yeah.net>
2025-11-27 17:10:48 +0800

    add api export post update

---
+++ docroot/modules/custom/xinshi_api/xinshi_api.post_update.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/**
+ * Udpate views date field custom formart with strip_tags.
+ */
+function xinshi_api_post_update_views_date_field_strip_tags() {
+  $views_list = \Drupal\views\Views::getViewsAsOptions();
+  foreach ($views_list as $name => $view) {
+    $vid = explode(':', $name)[0];
+    $display_id = explode(':', $name)[1];
+    /** @var \Drupal\views\ViewExecutable $view */
+    $view = \Drupal\views\Views::getView($vid);
+    $view->setDisplay($display_id);
+    $display_options = &$view->getDisplay()->options;
+    $changed = false;
+    foreach ($display_options['fields'] as $field_name => &$field_info) {
+      if (isset($field_info['settings']['date_format']) && $field_info['settings']['date_format'] == 'custom') {
+        $field_info['alter']['strip_tags'] = true;
+        $changed = true;
+      }
+    }
+    if ($changed) {
+      $view->getDisplay()->setOption('fields', $display_options['fields']);
+      $view->save();
+    }
+  }
+  \Drupal::service('cache.render')->invalidateAll();
+  \Drupal::service('cache.data')->invalidateAll();
+  \Drupal::service('cache.config')->invalidateAll();
+}
